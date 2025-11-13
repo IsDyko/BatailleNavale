6fa4a0ecd105eaffae00134decfb52d1b77c0748
Diogo Gonçalves Martins <pm71cef@eduvaud.ch>
2025-11-13 11:21:33 +0100

    generation bouttons

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,27 @@ namespace BattleShip
 {
     public partial class Form1 : Form
     {
+
+        //Creation des objets
         public Form1()
         {
             InitializeComponent();
+            int width = 50;
+            int space = 2;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button btnGame = new Button();
+                    btnGame.Text = "";
+                    btnGame.Width = width;
+                    btnGame.Height = width;
+                    btnGame.Left = j * (width + space);
+                    btnGame.Top = i * (width + space);
+                    this.Controls.Add(btnGame);
+                }
+            }
         }
     }
 }
---
+++ Grille.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace BattleShip
+{
+    internal class grille
+    {
+
+    }
+}
